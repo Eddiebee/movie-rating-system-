a1a31b9e7f2678501102ffdf14427f7f1efb9e0f
eddiebee <edbeeotudor@gmail.com>
2023-04-14 19:35:30 +0000

    Update 5 files

--- src/components/BaseDropdown.vue
+++ src/components/BaseDropdown.vue
@@ -1,5 +1,3 @@
-<script setup></script>
-
 <template>
   <button
     class="relative flex z-[2] flex items-center rounded bg-green-300 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -14,7 +14,9 @@ const props = defineProps<{
       class="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-emerald-50 bg-clip-padding px-3 py-4 text-sm font-normal leading-tight text-neutral-600 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
       :value="modelValue"
       :placeholder="label"
-      @input="$emit('update:modelValue', $event.target.value)"
+      @input="
+        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
     />
     <label
       for="floatingInput"
